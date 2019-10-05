65b757d3440a93b5be05a2445c2e3cbb35da0fe7
Jolanta Jas <jolantajas@Jolantas-MacBook-Pro.local>
2019-10-05 11:28:33 +0100

    adds axios and a call to API for random quote generator

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,44 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, ScrollView, Text, Button} from 'react-native'
+import axios from 'axios'
+import TheText from '../UI/TheText'
 
-
-import React, { useState } from 'react'
-import { Platform, StatusBar, StyleSheet, ScrollView, Text, Button} from 'react-native'
-
+const QUOTE_API = 'https://favqs.com/api/qotd'
 
 const Home = ({navigation}) => {
- 
-    const [counter, setCounter] = useState(0)
-    const { container } = styles
-    const {navigate} = navigation;
+  const [counter, setCounter] = useState(0)
+  const [quote, setQuote] = useState({})
 
-    return (
-      <ScrollView contentContainerStyle={container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <Text>Home</Text>
-        
-        <Button
-          title="Go to profile"
-          accessibilityLabel="Go to profile Button"
-          onPress={() => {
-            setCounter(counter + 1)
-            navigate('Profile', {name: 'Jane'})}
-          }
-        />
-        <Text>{counter}</Text>
-        <StatusBar/>
-        
-      </ScrollView>
-    )
-  
-  }
+  const { container } = styles
+  const {navigate} = navigation
+
+  useEffect(() => {
+    axios.get(QUOTE_API)
+    .then(res => setQuote(res.data.quote))
+    .catch(err => console.log('error getting quote ===>', err))
+  },[])
+
+
+  return (
+    <ScrollView contentContainerStyle={container}>
+      <TheText bold>Home</TheText>
+      <TheText>{quote.body}</TheText>
+      <TheText light>{quote.author}</TheText>
+
+      <Button
+        title="Go to profile"
+        accessibilityLabel="Go to profile Button"
+        onPress={() => {
+          setCounter(counter + 1)
+          navigate('Profile', {name: 'Jane'})}
+        }
+      />
+      <Text>{counter}</Text>
+      
+    </ScrollView>
+  )
+
+}
 
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#fff',
+    margin: 20
   },
 })
 
