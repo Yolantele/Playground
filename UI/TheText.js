b8506ac81bf59e3e3d0f0a70c25cb165d3db94b5
Jolanta Jas <jolantajas@Jolantas-MacBook-Pro.local>
2019-10-05 11:27:04 +0100

    defines TheText component as the solution to isolated fontFamily accorss the App - use instead of Text

---
+++ UI/TheText.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const DEFAULT = 15
+const TheText = ({children, bold, light, ...rest}) => {
+  const { text } = styles
+  const [fontSize, setFontSize] = useState(DEFAULT)
+
+  const fontWeight = () => {
+    if (bold)  return 'bold' 
+    if (light) return '100'
+    return '300'
+  }
+  
+  return( 
+    <Text 
+      style={{...text, fontSize, fontWeight: fontWeight() }} 
+      onLongPress={() => fontSize === DEFAULT ? setFontSize(18) : setFontSize(DEFAULT)}
+      {...rest}
+    >
+      {children}
+    </Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'helvetica',
+    color: 'darkslategrey',
+    lineHeight: 20,
+    textAlign: 'justify',
+    letterSpacing: 1
+  },
+})
+  
+export default TheText
