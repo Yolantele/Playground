707412afe5b0ec5a87f86518172e7409d7e2e461
Jolanta Jas <jolantajas@Jolantas-MacBook-Pro.local>
2019-11-13 21:50:02 +0000

    removes unused

--- App.jsx
+++ App.jsx
@@ -1,7 +1,3 @@
-import { Home } from './Screens/Home'
-import { Profile } from './Screens/Profile'
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
 import customTheme from './customTheme'
 import React from 'react'
 import { Provider } from '@ant-design/react-native'
