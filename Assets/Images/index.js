53a433acf81062933e30a0f927af206cc45710b9
Yolantele <klpdjolanta@gmail.com>
2019-12-15 16:05:34 +0000

    STYLE - loads more images for each step

--- Assets/Images/index.js
+++ Assets/Images/index.js
@@ -1,8 +1,11 @@
 export const ONBOARD = {
-  IMG_1: require('./glo1.png'),
-  IMG_2: require('./glo2.png'),
-  IMG_3: require('./glo3.png'),
-  IMG_4: require('./glo4.png'),
-  IMG_5: require('./glo5.png'),
-  IMG_6: require('./glo6.png')
+  IMG1: require('./glo1.png'),
+  IMG2: require('./glo2.png'),
+  IMG3: require('./glo3.png'),
+  IMG4: require('./glo4.png'),
+  IMG5: require('./glo5.png'),
+  IMG6: require('./glo6.png'),
+  IMG7: require('./glo7.png'),
+  IMG8: require('./glo8.png'),
+  IMG9: require('./glo9.png')
 }
