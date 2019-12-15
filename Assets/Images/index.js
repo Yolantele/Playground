1820911cc5b2783a498766bd6240d542454d03dc
Yolantele <klpdjolanta@gmail.com>
2019-12-15 15:06:48 +0000

    Additional Images for the app

--- Assets/Images/index.js
+++ Assets/Images/index.js
@@ -1,3 +1,8 @@
-export const ONBOARD = require('./glo1.png')
-export const ONBOARD_2 = require('./glo2.png')
-export const ONBOARD_3 = require('./glo3.png')
+export const ONBOARD = {
+  IMG_1: require('./glo1.png'),
+  IMG_2: require('./glo2.png'),
+  IMG_3: require('./glo3.png'),
+  IMG_4: require('./glo4.png'),
+  IMG_5: require('./glo5.png'),
+  IMG_6: require('./glo6.png')
+}
